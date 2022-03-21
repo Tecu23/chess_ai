7ce04da48a7072d933b4e39a98814697343520fe
Andrei-Raul Teculescu <andrei.teculescu@gmail.com>
2022-03-21 18:14:23 +0200

    update pawn moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -21,17 +21,93 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
+                              'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves }
         self.whiteToMove = True
         self.moveLog = []
 
+    # This will not work for castling, wn passant and pawn promotion
     def makeMove(self,move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
 
         self.moveLog.append(move) #log the move so we can undo it later
 
-        self.whiteToMove = False
-    
+        self.whiteToMove = not self.whiteToMove
+
+    """
+        Undo the last move made
+    """
+    def undoMove(self):
+        if len(self.moveLog) != 0: # make sure that there is a move to undo
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+
+    """
+        All moves considering checks
+    """
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    """
+        All moves without considering checks
+    """
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if (turn == "w" and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    self.moveFunctions[piece](r, c, moves)
+        return moves
+
+    """
+    Get all the pawn moves for the pawn located at row, col and add these moves to the list
+    """
+    def getPawnMoves(self, r, c, moves):
+        if self.whiteToMove: #white pawn moves
+            if self.board[r-1][c] == "--": # 1 square pawn advance
+                moves.append(Move((r,c),(r-1,c),self.board))
+                if r == 6 and self.board[r-2][c] == "--": # 2 square advance
+                    moves.append(Move((r,c),(r-2,c),self.board))
+            if c - 1 >= 0:
+                if self.board[r-1][c-1][0] == 'b': #enemy piece to capture
+                    moves.append(Move((r,c),(r-1,c-1),self.board))
+            if c + 1 <= 7:
+                if self.board[r-1][c+1][0] == 'b':
+                    moves.append(Move((r,c),(r-1,c+1),self.board))
+        else: # black pawn moves
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r,c),(r+1,c),self.board))
+                if r == 1 and self.board[r+2][c] == "--":
+                    moves.append(Move((r,c),(r+2,c),self.board))
+            if c - 1 >= 0:
+                if self.board[r+1][c-1][0] == 'w':
+                    moves.append(Move((r,c),(r+1,c-1),self.board))
+            if c + 1 <= 7:
+                if self.board[r+1][c+1][0] == 'w':
+                    moves.append(Move((r,c),(r+1,c+1),self.board))
+
+
+    """
+    Get all the rook moves for the pawn located at row, col and add these moves to the list
+    """
+    def getRookMoves(self, r, c, moves):
+        pass
+    def getKnightMoves(self, r, c, moves):
+        pass
+    def getBishopMoves(self, r, c, moves):
+        pass
+    def getQueenMoves(self, r, c, moves):
+        pass
+    def getKingMoves(self, r, c, moves):
+        pass
+
+
 class Move():
 
     # maps keys to values
@@ -54,6 +130,17 @@ class Move():
 
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+
+        self.moveId = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    """
+        Override the equals method
+    """
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveId == other.moveId
+        return False
+
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow,self.endCol)
--- ChessMain.py
+++ ChessMain.py
@@ -68,6 +68,10 @@ def main():
     screen.fill(p.Color("white"))
     
     gs = ChessEngine.GameState()
+
+    validMoves = gs.getValidMoves()
+
+    moveMade = False # flag variable for when a move is made
     
     load_images() # Only done once, before the while loop
     
@@ -81,6 +85,7 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            # mouse handler
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos() #(x,y) location of mouse
                 col = location[0]//SQ_SIZE
@@ -95,9 +100,21 @@ def main():
                 if len(playerClicks) == 2: # after thr second click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = () #reset user clicks
                     playerClicks = []
+            
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z: # undo when 'z' is pressed
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
 
         draw_game_state(screen,gs)
         clock.tick(MAX_FPS)
